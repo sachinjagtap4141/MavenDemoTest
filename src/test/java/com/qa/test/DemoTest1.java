a13a98c604d89297debd6fe06ca1af05fe5ceedf
sachinjagtap4141 <sachinjagtap4141@gmail.com>
2021-01-31 16:37:59 +0530

    Added new project

---
+++ src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,56 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+//	WebDriver driver;
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+		int a = 10;
+		int b = 20;
+//		System.setProperty("webdriver.chrome.driver", "D:\\DOCUMENTS\\SELENIUM JAR FILE AND CHORME DRI\\chromedriver_win32 (1)\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.quit();
+		Assert.assertEquals(30, a + b);
+
+	}
+
+	@Test
+	public void sub() {
+		System.out.println("sub");
+		int a = 10;
+		int b = 20;
+//		System.setProperty("webdriver.chrome.driver", "D:\\DOCUMENTS\\SELENIUM JAR FILE AND CHORME DRI\\chromedriver_win32 (1)\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.quit();
+		Assert.assertEquals(10, b - a);
+
+	}
+
+	@Test
+	public void div() {
+		System.out.println("Div");
+		int a = 10;
+		int b = 20;
+//		System.setProperty("webdriver.chrome.driver", "D:\\DOCUMENTS\\SELENIUM JAR FILE AND CHORME DRI\\chromedriver_win32 (1)\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.quit();
+		Assert.assertEquals(2, b / a);
+
+	}
+
+	@Test
+	public void mul() {
+		System.out.println("Mul");
+		int a = 10;
+		int b = 20;
+//		System.setProperty("webdriver.chrome.driver", "D:\\DOCUMENTS\\SELENIUM JAR FILE AND CHORME DRI\\chromedriver_win32 (1)\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.quit();
+		Assert.assertEquals(200, a * b);
+
+	}
+
+}
